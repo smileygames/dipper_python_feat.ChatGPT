8fc467c5da210ce1d4a4b3813c454452c8e955dd
smile <134200591+smileygames@users.noreply.github.com>
2023-06-14 15:08:00 +0900

    とりあえず、それっぽく動くようにはなった

--- dipper.py
+++ dipper.py
@@ -1,50 +1,54 @@
 import requests
 import configparser
 import subprocess
-import schedule
 import time
 import threading
 
+import requests
+
 class DDNSUpdater:
-    def __init__(self, username, password, domain):
+    def __init__(self, username, password, domain, use_ipv4=False, use_ipv6=False):
         self.username = username
         self.password = password
         self.domain = domain
+        self.use_ipv4 = use_ipv4
+        self.use_ipv6 = use_ipv6
     
-    def update_address(self, ip_address):
+    def update_address(self):
         raise NotImplementedError("Subclasses must implement update_address method.")
 
 class MyDNSUpdater(DDNSUpdater):
-    def __init__(self, username, password, ipv4_url, ipv6_url, domain):
-        super().__init__(username, password, domain)
+    def __init__(self, username, password, ipv4_url, ipv6_url, domain, use_ipv4=False, use_ipv6=False):
+        super().__init__(username, password, domain, use_ipv4, use_ipv6)
         self.ipv4_url = ipv4_url
         self.ipv6_url = ipv6_url
-        self.previous_ip = None
     
     def update_address(self, ipv4_address=None, ipv6_address=None):
-        if ipv6_address:
-            url = self.ipv6_url
-        else:
-            url = self.ipv4_url
-        
-        response = requests.get(url, auth=(self.username, self.password))
-        
+        if self.use_ipv4 and ipv4_address:
+            response = requests.get(self.ipv4_url, auth=(self.username, self.password))
+            
+        if self.use_ipv6 and ipv6_address:
+            response = requests.get(self.ipv6_url, auth=(self.username, self.password))
+            
         if response.status_code == 200:
             print("MyDNS address update successful.")
         else:
             print("Failed to update MyDNS address.")
 
 class GoogleDomainsUpdater(DDNSUpdater):
-    def __init__(self, username, password, url, domain):
-        super().__init__(username, password, domain)
+    def __init__(self, username, password, url, domain, use_ipv4=False, use_ipv6=False):
+        super().__init__(username, password, domain, use_ipv4, use_ipv6)
         self.url = url
-        self.previous_ip = None
-    
-    def update_address(self, ip_address):
-        url = f"{self.url}?hostname={self.domain}&myip={ip_address}"
-        
-        response = requests.get(url, auth=(self.username, self.password))
-        
+  
+    def update_address(self, ipv4_address=None, ipv6_address=None):
+        if self.use_ipv4 and ipv4_address:
+            url = f"{self.url}?hostname={self.domain}&myip={ipv4_address}"
+            response = requests.get(url, auth=(self.username, self.password))
+
+        elif self.use_ipv6 and ipv6_address:
+            url = f"{self.url}?hostname={self.domain}&myip={ipv6_address}"
+            response = requests.get(url, auth=(self.username, self.password))
+
         if response.status_code == 200:
             if response.text.startswith("good") or response.text.startswith("nochg"):
                 print("Google Domains address update successful.")
@@ -54,52 +58,67 @@ class GoogleDomainsUpdater(DDNSUpdater):
             print("Failed to update Google Domains address.")
 
 # 自分のIPアドレスを取得する関数
-def get_my_ip():
-    result = subprocess.run(["dig", "@ident.me", "-4", "+short"], capture_output=True, text=True)
+def get_my_ip(ipv6=False):
+    if ipv6:
+        result = subprocess.run(["dig", "@ident.me", "-6", "+short"], capture_output=True, text=True)
+    else:
+        result = subprocess.run(["dig", "@ident.me", "-4", "+short"], capture_output=True, text=True)
+    
     if result.returncode == 0:
         return result.stdout.strip()
-    else:
-        raise Exception("Failed to get IP address.")
+#    else:
+#        raise Exception("Failed to get IP address.")
 
 # ドメインのIPアドレスを取得する関数
-def get_domain_ip(domain):
-    result = subprocess.run(["dig", domain, "-4", "+short"], capture_output=True, text=True)
+def get_domain_ip(domain, ipv6=False):
+    if ipv6:
+        result = subprocess.run(["dig", domain, "AAAA", "+short"], capture_output=True, text=True)
+    else:
+        result = subprocess.run(["dig", domain, "A", "+short"], capture_output=True, text=True)
+    
     if result.returncode == 0:
         return result.stdout.strip()
-    else:
-        raise Exception("Failed to get domain IP address.")
+#    else:
+#        raise Exception("Failed to get domain IP address.")
 
 # 定期的なアドレス通知処理
-def update_ddns(domain_user):
-    # 自分のIPアドレスを取得
-    my_ip = get_my_ip()
-
-    # MyDNSのアップデート
-    domain_user.update_address(my_ip)
+def update_ddns(domain_user, use_ipv4=False, use_ipv6=False):
+    if use_ipv4:
+        my_ipv4 = get_my_ip()
+        domain_user.update_address(ipv4_address=my_ipv4)
+    if use_ipv6:
+        my_ipv6 = get_my_ip(ipv6=True)
+        domain_user.update_address(ipv6_address=my_ipv6)
 
 # 定期的なチェック処理
-def check_ddns(domains_user):
-    # ドメインのIPアドレスを取得
-    mydns_domain_ip = get_domain_ip(domains_user.domain)
-
-    # IPアドレスが変更された場合にアドレスを更新
-    my_ip = get_my_ip()
-    if my_ip != mydns_domain_ip:
-        domains_user.update_address(my_ip)
+def check_ddns(domain_user, use_ipv4=False, use_ipv6=False):
+    if use_ipv4:
+        domain_ipv4 = get_domain_ip(domain_user.domain)
+        my_ipv4 = get_my_ip()
+        if my_ipv4 != domain_ipv4:
+            domain_user.update_address(ipv4_address=my_ipv4)
+    if use_ipv6:
+        domain_ipv6 = get_domain_ip(domain_user.domain, ipv6=True)
+        my_ipv6 = get_my_ip(ipv6=True)
+        if my_ipv6 != domain_ipv6:
+            domain_user.update_address(ipv6_address=my_ipv6)
 
 # 別スレッド非同期タイマー処理
-def timer_update(interval_time, mydns_users, google_domains_users):
+def timer_update(interval_time, mydns_users, use_ipv4=False, use_ipv6=False):
     while True:
         for mydns_user in mydns_users:
-            update_ddns(mydns_user)
+            update_ddns(mydns_user, use_ipv4=use_ipv4, use_ipv6=use_ipv6)
+
         time.sleep(interval_time)
 
-def timer_check(interval_time, mydns_users, google_domains_users):
+def timer_check(interval_time, mydns_users, google_domains_users, use_ipv4=False, use_ipv6=False):
     while True:
         for mydns_user in mydns_users:
-            check_ddns(mydns_user)
+            user=mydns_user
+            check_ddns(user, user.use_ipv4, user.use_ipv6)
         for google_domains_user in google_domains_users:
-            check_ddns(google_domains_user)
+            user=google_domains_user
+            check_ddns(user, user.use_ipv4, user.use_ipv6)
 
         time.sleep(interval_time)
 
@@ -118,7 +137,9 @@ def main():
             ipv4_url = config.get(section, 'ipv4_url')
             ipv6_url = config.get(section, 'ipv6_url')
             domain = config.get(section, 'domain')
-            mydns = MyDNSUpdater(username, password, ipv4_url, ipv6_url, domain)
+            use_ipv4 = config.getboolean(section, 'use_ipv4')
+            use_ipv6 = config.getboolean(section, 'use_ipv6')
+            mydns = MyDNSUpdater(username, password, ipv4_url, ipv6_url, domain, use_ipv4, use_ipv6)
             mydns_users.append(mydns)
     
     # Google Domainsの設定を取得
@@ -129,20 +150,23 @@ def main():
             password = config.get(section, 'password')
             url = config.get(section, 'url')
             domain = config.get(section, 'domain')
-            google_domains = GoogleDomainsUpdater(username, password, url, domain)
+            use_ipv4 = config.getboolean(section, 'use_ipv4')
+            use_ipv6 = config.getboolean(section, 'use_ipv6')
+            google_domains = GoogleDomainsUpdater(username, password, url, domain, use_ipv4, use_ipv6)
             google_domains_users.append(google_domains)
+
 
     # アドレス通知処理の非同期実行
     notification_interval = int(config.get('Schedule', 'notification_interval_sec'))
     notification_threads = []
-    thread = threading.Thread(target=timer_update, args=(notification_interval, mydns_users, google_domains_users,))
+    thread = threading.Thread(target=timer_update, args=(notification_interval, mydns_users, use_ipv4, use_ipv6))
     thread.start()
     notification_threads.append(thread)
 
     # アドレスチェック処理の非同期実行
     check_interval = int(config.get('Schedule', 'check_interval_sec'))
     check_threads = []
-    thread = threading.Thread(target=timer_check, args=(check_interval, mydns_users, google_domains_users,))
+    thread = threading.Thread(target=timer_check, args=(check_interval, mydns_users, google_domains_users, use_ipv4, use_ipv6))
     thread.start()
     check_threads.append(thread)
 
